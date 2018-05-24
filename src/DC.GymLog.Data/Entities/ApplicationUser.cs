e37bd8590593b62bd6c1b287f7c2b8d86ac163c7
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2018-05-24 18:54:49 +0200

    Initialize projects

---
+++ src/DC.GymLog.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DC.GymLog.Data.Entities
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
